b3f3044771b3073a89d70cafac9ca994a11d4faf
manuella okai <manuellaokai87@gmail,com>
2024-10-21 14:35:55 +0000

    learning bash scripting

--- Bash_Logic_Operations/greet_user.sh
+++ Bash_Logic_Operations/greet_user.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <First Name> <Last Name>"
-    exit 1
-fi 
-
+#!/bin/bash 
 first_name=$1
 last_name=$2
-echo "Hello, $first_name $last_name! Welcome to Bash scripting."
+echo "Hello, $first_name $last_name!, Welcome to Bash scripting"
