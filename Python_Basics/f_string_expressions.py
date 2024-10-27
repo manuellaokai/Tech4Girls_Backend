877a3b39f3edeffcd4bbff297910d88c9b31d41f
manuella okai <manuellaokai87@gmail,com>
2024-10-27 18:24:03 +0000

    learning python basics

---
+++ Python_Basics/f_string_expressions.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+a = 10
+b = 20 
+
+print(f"The sum of {a} and {b} is {a + b}.")
+print(f"The difference when {b} is subtracted from {a} is {a - b}.")
+print(f"The product of {a} and {b} is {a * b}.")
+
