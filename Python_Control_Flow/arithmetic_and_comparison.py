a463a30efe98a92d1c2d14e00edc10dfb4147506
manuella okai <manuellaokai87@gmail,com>
2024-10-31 01:26:50 +0000

    learning control flow

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,46 @@
+#!usr/bin/python3
+
+x = 10
+y = 3
+
+# Perform calculations
+addition = (x + y)
+print(f"Addition (x + y): {addition}")
+
+subtraction = ( x - y)
+print(f"Subtraction (x - y): {subtraction}")
+
+multiplication = (x * y)
+print(f"Multiplication (x * y): {multiplication}")
+
+division = ( x / y)
+print(f"Division (x / y): {division}")
+
+floor_division = (x // y)
+print(f"Floor Division (x // y): {floor_division}")
+
+exponentiation = ( x ** y)
+print(f"Exponentiation (x ** y): {exponentiation}")
+
+modulus = (x % y)
+print(f"Modulus (x % y): {modulus}")
+
+
+# Perform comparisons
+equals = (x == y)
+print(f"Equals (x == y): {equals}")
+
+not_equals = (x != y)
+print(f"Not Equals (x != y): {not_equals}")
+
+less_than = (x < y)
+print(f"Less Than (x < y): {less_than}")
+
+greater_than = (x > y)
+print(f"Greater Than (x > y): {greater_than}")
+
+less_than_or_equal = (x <= y)
+print(f"Less Than or Equal (x <= y): {less_than_or_equal}")
+
+greater_than_or_equal = (x >= y)
+print(f"Greater Than or Equal (x >= y): {greater_than_or_equal}")
